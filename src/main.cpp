f778f7e961ce617217c6518c19a657ddfa31eed2
StefanDan <stefan.dan@tum.de>
2024-11-26 21:34:56 +0100

    example

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,26 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-   auto result = perlin::random3DVector(42);
-   for (const auto& x : result) {
-      std::cout << x << " ";
-   }
-   std::cout << std::endl;
+   // const unsigned chunkSize = 16;
+   // const unsigned nChunksX = 16;
+   // const unsigned nChunksY = 16;
+   // perlin::initialize2DGradients(chunkSize);
+   // perlin::matrix result(nChunksX * chunkSize, std::vector<double>(nChunksY * chunkSize, 0.0));
+   // const std::vector<unsigned> permutationTable = perlin::generatePermutationTable(42, chunkSize);
+   // for (unsigned i = 0; i < nChunksX; i++) {
+   //    for (unsigned j = 0; j < nChunksY; j++) {
+   //       perlin::fill2DChunk(result, i, j, permutationTable);
+   //    }
+   // }
+
+   // render::writeMatrixToFile(render::normalizeMatrix(result), "output16x16x16.txt");
 
    auto permutationTable = perlin::generatePermutationTable(42);
    auto noise = perlin::Noise2D(5.0, 7.0, permutationTable);
    std::cout << noise << std::endl;
 
-   std::vector<double> noise2DExample = { -1.0, -0.8, -0.4, -0.2, 0.0,
-                                          0.2, 0.4, 0.6, 0.8, 1 };
+   std::vector<double> noise2DExample = {-1.0, -0.8, -0.4, -0.2, 0.0,
+                                         0.2, 0.4, 0.6, 0.8, 1};
    render::ppm(noise2DExample, 2, 5);
 
    return 0;
