579a11187ff1486de88bae6414e9345ddf61327a
StefanDan <stefan.dan@tum.de>
2024-12-02 20:55:23 +0100

    added meshes

--- include/PerlinAlgorithm.hpp
+++ include/PerlinAlgorithm.hpp
@@ -63,19 +63,21 @@ void initialize2DGradients(const int numGradients, UniformUnitGenerator& unif = 
 /// @brief Compute the Perlin Noise value of a pixel.
 /// @param x x-coordinate within chunk
 /// @param y y-coordinate within chunk
+/// @param chunkSize the size of a chunk
 /// @param chunkX x-coordinate of the chunk within the entire grid
 /// @param chunkY y-coordinate of the chunk within the entire grid
 /// @param permutationTable the permutation table used to pick gradients
 /// @return the Perlin Noise value of the pixel result[chunkX*chunkSize+x][chunkY*chunkSize+y]
-double noise2D(const unsigned x, const unsigned y, const unsigned chunkX, const unsigned chunkY, const std::vector<unsigned>& permutationTable);
+double noise2D(const unsigned x, const unsigned y, const unsigned chunkSize, const unsigned chunkX, const unsigned chunkY, const std::vector<unsigned>& permutationTable);
 
 /// @author SD
 /// @brief Fill a chunk with Perlin Noise Values.
 /// @param resultMatrix the final output -- to be filled
+/// @param chunkSize the size of a chunk
 /// @param chunkX x-coordinate of the chunk within the entire grid
 /// @param chunkY y-coordinate of the chunk within the entire grid
 /// @param permutationTable the permutation table used to pick gradients
-void fill2DChunk(matrix& resultMatrix, const unsigned chunkX, const unsigned chunkY, const std::vector<unsigned>& permutationTable);
+void fill2DChunk(matrix& resultMatrix, const unsigned chunkSize, const unsigned chunkX, const unsigned chunkY, const std::vector<unsigned>& permutationTable);
 
 /// @author SD
 /// @brief Prints the values of `matrix` to standard output stream
@@ -86,8 +88,12 @@ void printMatrix(const matrix& matrix);
 /// @brief Return the index corresponding to the 2D gradient for the corner of the grid given by `chunkX` and `chunkY`.
 /// @param chunkX x-coordinate of the chunk within the entire grid
 /// @param chunkY y-coordinate of the chunk within the entire grid
+/// @param chunkSize the size of a chunk
 /// @param permutationTable the permutation table used to pick gradients
 unsigned permutationValue2D(const unsigned chunkX, const unsigned chunkY, const std::vector<unsigned>& permutationTable);
+
+/// work in progress
+matrix stackNoise(std::vector<matrix>& matrices, std::vector<double>& weights);
 
 //------- General Functions -------
 double dot(std::vector<double> u, std::vector<double> v);
