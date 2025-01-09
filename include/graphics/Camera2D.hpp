74a745e4dc6d4bf5737db5fa1c17212975539fbd
Pawel Adam Kedzia <pawel.kedzia@tum.de>
2025-01-09 20:01:51 +0100

    Implemented camera controls in Camera2D.cpp

--- include/graphics/Camera2D.hpp
+++ include/graphics/Camera2D.hpp
@@ -6,12 +6,17 @@
 class Camera2D final : public Camera {
    public:
    glm::vec3 Right = glm::vec3(1.0f, 0.0f, 0.0f);
+
+   double lastMouseX = 0.0f;
+   double lastMouseY = 0.0f;
+
    Camera2D(int width, int height, glm::vec3 position, GLFWwindow* window): Camera(width, height, position) {
       glfwSetWindowUserPointer(window, this);
    };
 
    void Inputs(GLFWwindow* window) override;
-   static void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+   // This function is static because it is a callback, and will not work otherwise.
+   static void ScrollCallback(GLFWwindow* window, double xOffset, double yOffset);
 };
 
 #endif //INC_2DCAMERA_HPP
--- src/graphics/Camera2D.cpp
+++ src/graphics/Camera2D.cpp
@@ -19,12 +19,41 @@ void Camera2D::Inputs(GLFWwindow* window) {
    }
 
    glfwSetScrollCallback(window, ScrollCallback);
+
+   // Mouse related operations
+   if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+      // GLFW_CURSOR_DISABLED hides the cursor and allows for unlimited movement
+      glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+      if (glfwRawMouseMotionSupported()) // If supported, use raw mouse motion, disabling acceleration
+         glfwSetInputMode(window, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
+
+      if (firstClick) {
+         // Prevents camera from jumping on the first click
+         glfwGetCursorPos(window, &lastMouseX, &lastMouseY);
+         firstClick = false;
+      }
+
+      // Update the cursor position variables
+      glfwGetCursorPos(window, &mouseX, &mouseY);
+      // Calculate delta
+      const double deltaX = (mouseX - lastMouseX) / 50;
+      const double deltaY = (mouseY - lastMouseY) / 50;
+      // Update last position
+      lastMouseX = mouseX;
+      lastMouseY = mouseY;
+      // Change position
+      Position += speed * glm::vec3(-deltaX, deltaY, 0.0f);
+   } else if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_RELEASE) {
+      glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+      firstClick = true;
+   }
 }
 
-void Camera2D::ScrollCallback(GLFWwindow* window, double xoffset, double yoffset) {
-   Camera2D* camera = static_cast<Camera2D*>(glfwGetWindowUserPointer(window));
+void Camera2D::ScrollCallback(GLFWwindow* window, double xOffset, double yOffset) {
+   auto camera = static_cast<Camera2D*>(glfwGetWindowUserPointer(window));
    if (camera == nullptr) return;
 
    // Consider clamping function here
-   camera->Position += camera->speed * glm::vec3(0.0f, 0.0f, -yoffset);
+   camera->Position += camera->speed * glm::vec3(0.0f, 0.0f, -yOffset);
 }
